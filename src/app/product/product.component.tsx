d7dba501f9a0f2cb95f63f5607e315aa3543aa6f
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-05-09 12:03:38 +0200

    🌈 Added back to products icon

--- src/app/product/product.component.tsx
+++ src/app/product/product.component.tsx
@@ -14,6 +14,7 @@ import ReactMarkdown from 'react-markdown';
 import { ProductCounter } from '../../ui/product-counter/product-counter.component';
 import Link from 'next/link';
 import { getLink, AppRoute } from '../../router/app-routes';
+import { LeftOutlined } from '@ant-design/icons';
 
 interface ProductProps {
   name: string;
@@ -76,6 +77,7 @@ export const Product = ({
             initial="initial"
             animate="animate"
           >
+            <LeftOutlined />
             Back to products
           </BackToProductsLink>
         </Link>
