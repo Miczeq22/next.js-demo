db2731abb00348189d856e8b16a8537aafd65611
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-05-11 07:57:31 +0200

    ✨ Cart item list component

---
+++ src/app/cart/cart-item-list/cart-item-list.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { CartItemList } from './cart-item-list.component';
+import { AppProvider } from '../../../providers/app.provider';
+
+export default {
+  title: 'Cart Item List',
+  component: CartItemList,
+  decorators: [(story) => <AppProvider>{story()}</AppProvider>],
+};
+
+export const EmptyList = () => <CartItemList items={[]} />;
+
+export const Default = () => (
+  <CartItemList
+    items={[
+      {
+        count: 2,
+        price: 19999,
+        imageUrl:
+          'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        name: 'Product No.1',
+      },
+      {
+        count: 5,
+        price: 199999,
+        imageUrl:
+          'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        name: 'Product No.2',
+      },
+      {
+        count: 1,
+        price: 1999999,
+        imageUrl:
+          'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        name: 'Product No.3',
+      },
+    ]}
+  />
+);
---
+++ src/app/cart/cart-item/cart-item.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CartItem } from './cart-item.component';
+import { AppProvider } from '../../../providers/app.provider';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Cart Item',
+  component: CartItem,
+  decorators: [withKnobs, (story) => <AppProvider>{story()}</AppProvider>],
+};
+
+export const Default = () => (
+  <CartItem
+    count={number('count', 1, {
+      min: 1,
+    })}
+    price={number('price', 29999, {
+      min: 1,
+    })}
+    imageUrl="https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    name={text('name', 'Product No.1')}
+  />
+);
